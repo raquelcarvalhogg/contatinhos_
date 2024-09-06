8fe2a22427bf23332380bc02c782d40a7cb24a68
raquelcarvalhogg <rachelcarvalho2006@gmail>
2024-09-06 13:34:48 -0300

    app para validar as alterações.

--- contatinhos/App.tsx
+++ contatinhos/App.tsx
@@ -1,5 +1,21 @@
+import {
+  useFonts,
+  Ubuntu_700Bold,
+  Ubuntu_500Medium,
+  Ubuntu_400Regular
+} from '@expo-google-fonts/ubuntu'
+
 import { Home } from '@/app/home';
+import { Loading } from '@/app/components/loading';
 
 export default function App() {
-  return <Home />
+  const [fontsLoaded] = useFonts({
+    Ubuntu_700Bold,
+    Ubuntu_500Medium,
+    Ubuntu_400Regular
+  })
+
+  if (!fontsLoaded){
+    return <Loading />
+  }
 }
