28e1da06f5729ac13b8185dd6338838590a45be9
raquelcarvalhogg <rachelcarvalho2006@gmail>
2024-09-06 13:50:24 -0300

    alteração das cores.

--- contatinhos/src/app/components/loading/index.tsx
+++ contatinhos/src/app/components/loading/index.tsx
@@ -1,9 +1,11 @@
 import { ActivityIndicator } from 'react-native'
+import { theme } from '@/theme'
 
 import { styles } from './styles'
 
 export function Loading() {
     return (
-        <ActivityIndicator style={styles.Loading}/>
+        <ActivityIndicator style={styles.Loading}
+        color={theme.colors.blue}/>
     )
 }
